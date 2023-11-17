ff929ea3ec881b7837f910c0b925b48a8cb23059
Selin Namak <selin-guzel@hotmail.com>
2023-11-17 23:37:42 +0300

    shop page oluşturulmaya başlandı, models uygulamasına geçildi

--- lib/pages/shop_page.dart
+++ lib/pages/shop_page.dart
@@ -1,3 +1,4 @@
+import 'package:e_commerce/components/shoe_tile.dart';
 import 'package:flutter/material.dart';
 
 class ShopPage extends StatefulWidget {
@@ -10,6 +11,64 @@ class ShopPage extends StatefulWidget {
 class _ShopPageState extends State<ShopPage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Column(
+      children: [
+        Container(
+          padding: const EdgeInsets.all(12.0),
+          margin: const EdgeInsets.symmetric(horizontal: 25.0),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(8.0),
+            color: Colors.grey[200],
+          ),
+          child: const Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                'Search',
+                style: TextStyle(color: Colors.grey),
+              ),
+              Icon(
+                Icons.search,
+                color: Colors.grey,
+              ),
+            ],
+          ),
+        ),
+        const Padding(
+          padding: EdgeInsets.all(25.0),
+          child: Text(
+            'everyone flies.. some fly longer than others',
+            style: TextStyle(color: Colors.grey),
+          ),
+        ),
+        const Padding(
+          padding: EdgeInsets.symmetric(horizontal: 25.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              Text(
+                'Hot Picks 🔥',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+              ),
+              Text(
+                'See All',
+                style: TextStyle(color: Colors.blue),
+              )
+            ],
+          ),
+        ),
+        const SizedBox(
+          height: 10,
+        ),
+        Expanded(
+          child: ListView.builder(
+            itemBuilder: (context, index) {
+              return ShoeTile();
+            },
+          ),
+        ),
+      ],
+    );
   }
 }
